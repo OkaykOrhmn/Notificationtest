6ee046de5a53aeaab550006950f34c0e5cae3107
KeyRhmn01 <kiainsta22@gmail.com>
2022-08-10 12:24:12 +0430

    Initial commit

---
+++ app/src/main/java/com/notif/notificationtest/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.notif.notificationtest;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
